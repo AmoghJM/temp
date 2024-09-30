247c24d5dde33a0ca83ba5a827b82f3774c2bafc
AmoghJM <amoghmoger@gmail.com>
2024-09-30 18:50:54 +0530

    Fixed

--- ArrayOfStrings.java
+++ ArrayOfStrings.java
@@ -6,12 +6,19 @@ public class ArrayOfStrings {
         System.out.println("Enter length of array:");
         int length = sc.nextInt();
         String[] arr = new String[length];
-        for(int i=0;i<length;i++) {
+        for (int i = 0; i < length; i++) {
             arr[i] = sc.next();
         }
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.next();
+        }
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.next();
+        }
+
         int sum = 0;
         System.out.println("Cumulative length of all the strings are:");
-        for(int i=0;i<length;i++) {
+        for (int i = 0; i < length; i++) {
             sum = sum + arr[i].length();
         }
         System.out.println(sum);
