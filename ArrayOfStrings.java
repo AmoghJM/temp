998913b4694506b228883f89623ad80ee953e2b9
AmoghJM <amoghmoger@gmail.com>
2024-09-30 18:48:38 +0530

    First Commit

---
+++ ArrayOfStrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayOfStrings {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of array:");
+        int length = sc.nextInt();
+        String[] arr = new String[length];
+        for(int i=0;i<length;i++) {
+            arr[i] = sc.next();
+        }
+        int sum = 0;
+        System.out.println("Cumulative length of all the strings are:");
+        for(int i=0;i<length;i++) {
+            sum = sum + arr[i].length();
+        }
+        System.out.println(sum);
+    }
+}
